d7fac2c38f8a13d25161ce8bad0e5f48c3f1c3dc
David <itsdavid71@gmail.com>
2022-07-28 21:58:02 +0300

    date-fns

--- components/Post.tsx
+++ components/Post.tsx
@@ -11,17 +11,22 @@ import Link from "next/link";
 import IconButton, { IconButtonProps } from "@mui/material/IconButton";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import type PostType from "../types/post";
+import { formatDistance } from "date-fns";
+import { ru } from "date-fns/locale";
 
 type PostPropTypes = {
   post: PostType;
 };
 const Post: FC<PostPropTypes> = ({ post }) => {
+  const date = post.createdAt
+    ? formatDistance(post.createdAt, new Date(), {
+        addSuffix: true,
+        locale: ru,
+      })
+    : "";
   return (
     <Card>
-      <CardHeader
-        title={post.user.name}
-        subheader={post.createdAt ? post.createdAt?.toLocaleDateString() : ""}
-      />
+      <CardHeader title={post.user.name} subheader={date} />
       <Link href={`/posts/${post.id}`}>
         <a>
           <CardMedia component="img" image={post.imageURL} />
