4e9a95ec1011b6a2c4badaa3490dd6027d5e50ae
David <itsdavid71@gmail.com>
2022-08-09 20:03:27 +0300

    Fix

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -1,28 +1,9 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
-import {
-  collection,
-  query,
-  where,
-  getDocs,
-  orderBy,
-  updateDoc,
-  doc,
-  limit,
-  increment,
-} from "firebase/firestore";
-import {
-  useCollectionData,
-  useDocumentData,
-} from "react-firebase-hooks/firestore";
+import { collection, query, orderBy } from "firebase/firestore";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 import { auth, db } from "../app/firebaseApp";
-import Link from "next/link";
-import styles from "../styles/Home.module.css";
-import { style } from "@mui/system";
 import postConverter from "../helpers/postConverter";
-import userConverter from "../helpers/userConverter";
-import PostType from "../types/post";
-import { useAuthState } from "react-firebase-hooks/auth";
 import { Card, Button, Box } from "@mui/material";
 import PostContainer from "../containers/PostContainer";
 
@@ -41,15 +22,9 @@ const Posts: NextPage = () => {
       {posts && (
         <div>
           {posts.map((post) => (
-            // <Link
-            //   key={post.id}
-            //   className={styles.postLink}
-            //   href={"/posts/" + post.id}
-            // >
-            <Box sx={{ mb: 2, mt: 2 }}>
+            <Box key={post.id} sx={{ mb: 2, mt: 2 }}>
               <PostContainer post={post} />
             </Box>
-            // </Link>
           ))}
         </div>
       )}
